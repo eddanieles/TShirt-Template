dfa79fb8e92fac1d98988072cce0722bb3a1a011
eddanieles <ed.danieles@yahoo.com>
2016-08-04 20:27:19 -0400

    attempted another page

--- app.js
+++ app.js
@@ -18,9 +18,38 @@ $(document).ready(function(){
   $(".shirt").click(function(){
     $(".content").hide();
     $("h1").text("Shirt");
-    $(".shirt_page").show();
+    shirt_display($(this));
   })
 
+  function shirt_display (shirt) {
+    var shirt = $(`<h2>${shirt.name}</h2>
+
+    <div class="shirt_content">
+
+    <section>
+      <img src=${shirt.scr} alt="${shirt.name}" />
+    </section>
+
+    <section>
+      <ul>
+        <li>${shirt.description}</li>
+        <li>Price: $${shirt.price}</li>
+        <li class="sizes">
+          Size
+          <ul>
+            <li>S</li>
+            <li>M</li>
+            <li>L</li>
+            <li>XL</li>
+          </ul>
+        </li>
+        <li>Rating: ${shirt.rating}</li>
+        <li><button type="button" name="button">Add to Cart</button></li>
+      </ul>
+    </section>`);
+    $(".shirt_page").append(shirt);
+
+  }
 
 
 
--- data.js
+++ data.js
@@ -4,20 +4,23 @@ var data = [
     name: "Save my trees",
     price: 29,
     description: "Help save the rainforest. With every purchase a new tree will be planted.",
-    rating: "4 Stars"
+    rating: "4 Stars",
+    id: 1
   },
   {
     scr: "./images/pink-front.png",
     name: "Nature Lover",
     price: 19,
     description: "The perfect shirt for taking a stroll through the woods.",
-    rating: "4.5 Stars"
+    rating: "4.5 Stars",
+    id: 2
   },
   {
     scr: "./images/yellow-front.png",
     name: "Forest Walk",
     price: 39,
     description: "Forege berries in this shirt.",
-    rating: "5 Stars"
+    rating: "5 Stars",
+    id: 3
   }
 ];
